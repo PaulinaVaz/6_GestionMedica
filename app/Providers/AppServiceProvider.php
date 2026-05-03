3dfa2fd92a851092dbc2116135c49e1833405e91
NJ-JEET <cg750431@gmail.com>
2026-05-02 21:36:48 -0600

    Fix: Forzando HTTPS y configurando TrustProxies para Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Importante agregar esta línea
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
-        //
+        // Forzar HTTPS si estamos en Heroku (entorno de producción)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
